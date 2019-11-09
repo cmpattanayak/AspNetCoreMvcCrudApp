5a94ae7c939825da1b2bc426ca82f8fa404cc154
Chinmaya Pattanayak <cmpattanayak@gmail.com>
2019-11-09 19:42:28 +0530

    All files added

---
+++ AspNetCoreMvcCrudApp/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreMvcCrudApp.DataContext;
+using AspNetCoreMvcCrudApp.Models;
+
+namespace AspNetCoreMvcCrudApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
+
+        public IActionResult AddEdit(int id = 0)
+        {
+            if (id > 0)
+                return View(_context.Employees.Find(id));
+
+            return View(new Employee());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddEdit([Bind("EmployeeId,Name,EmpCode,Position,OfficeLocation")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                if (employee.EmployeeId > 0)
+                    _context.Update(employee);
+                else
+                    _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ AspNetCoreMvcCrudApp/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMvcCrudApp.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
+        [Display(Name="Full Name")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(4)")]
+        [Display(Name="Employee Code")]
+        public string EmpCode { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Position { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [Display(Name="Office Location")]
+        public string OfficeLocation { get; set; }
+    }
+}
